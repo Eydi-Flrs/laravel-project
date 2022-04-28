938a8adb05c479425cdd97fb38478468eba5558f
Eydi-Flrs <adrienneflores44@gmail.com>
2022-04-28 22:28:40 +0800

    KMS delete modal for archive

--- resources/views/admin/posts/archived.blade.php
+++ resources/views/admin/posts/archived.blade.php
@@ -67,13 +67,31 @@
                                         </form>
                                     </td>
                                     <td>
-                                        {{--                                    @can('view',$post)--}}
-                                        <form method="post" action="{{route('post.destroy',$post->id)}}" enctype="multipart/form-data">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit" class="btn btn-danger">Delete</button>
-                                        </form>
-                                        {{--                                    @endcan--}}
+                                        <a class="btn btn-danger" href="#" data-toggle="modal" data-target="#deleteModal">
+                                            Delete
+                                        </a>
+                                        <!-- Delete Modal-->
+                                        <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                            <div class="modal-dialog" role="document">
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <h5 class="modal-title" id="exampleModalLabel">Are sure you want to permanently delete "{{strtoupper($post->title)}}"?</h5>
+                                                        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                                                            <span aria-hidden="true">×</span>
+                                                        </button>
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                                                        <form method="post" action="{{route('post.destroy',$post->id)}}" enctype="multipart/form-data">
+                                                            @csrf
+                                                            @method('DELETE')
+                                                            <button type="submit" class="btn btn-danger">Delete</button>
+                                                        </form>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
                                     </td>
                                 </tr>
 
